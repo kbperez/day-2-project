d82d2fa3279c4cf38dbf1ddaf7cf17f3961dbfa6
kbperez <kbpgperez@hotmail.com>
2017-10-19 00:54:30 -0700

    Added number guessing, state guessing, Top Ten, ul and ol, some color

--- app.js
+++ app.js
@@ -66,3 +66,68 @@ if (fellow === 'y' || fellow === 'yes') {
 } else {
 alert ('Actually, Karen is studying at CodeFellows and working hard.  Thank you for taking the time to get to know Karen.');
 }
+
+// Guess a number between 0-10, unlimited tries
+var number;
+var rando = Math.floor(Math.random() * 11); //Random number 0-10
+var counter = 0;
+
+while (number !== rando) { //While loop to keep guessing
+  number = parseInt(prompt('Guess a number from 0-10?'));
+  console.log('Guessed', number);
+
+  if (isNaN(number)) {
+    alert ('Your response was not a number.  Please enter a number.');
+
+  }else if (number < rando) {
+    alert ('Sorry you guessed too low.');
+    console.log('counter:', counter);
+
+  } else if (number > rando) {
+    alert ('Sorry you guessed too high.');
+    console.log('counter:', counter);
+
+  }
+
+  counter++;
+  console.log('counter:', counter);
+}
+
+  alert ('You got it right!  Number of tries was ' + counter);
+  console.log('counter:', counter)
+
+
+// Array of states I've lived or worked in. -->
+var states = ['MI' ,'GA' ,'SC' ,'NY', 'OK', 'CT', 'WI'];
+var flag = false;
+var guess = prompt('Can you guess a state other than WA that I have lived or worked in?  You get 6 tries.  Use the 2 letter postal abbreviation for your answer.');
+
+guess = guess.toUpperCase(); //Alway upper case
+
+for (var j = 0; j<5; j++) { //Outer loop for tries
+
+console.log('Outer loop', guess, j)
+
+     for (var i =0; i<states.length; i++) { //Inner loop to check array
+      console.log('Inner loop', states[i],flag)
+
+      if (guess === states[i]) { //If the correct answer do stuff
+        console.log('Correct');
+        alert('Great job! You are correct.');
+        flag = true;
+        j = 99;
+        break;
+
+      }
+
+    }
+  if (!flag) { //To reguess
+  var guess = prompt('Sorry that\'s wrong.  ' + (5-j) + ' more attempts. Try again.');
+  guess = guess.toUpperCase(); //Alway uoper case
+  console.log('reset',guess);
+}
+}
+
+if (!flag) { //If you didn't guess the state in 6 tries
+  alert('Sorry, no more tries.  The states I\'ve lived or worked in are MI, GA, SC, NY, OK, CT, WI.');
+} //END or prompts
